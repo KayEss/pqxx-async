8684ac3d056666038a2989756668f8779900cf73
Kirit Sælensminde <kirit@kirit.com>
2021-10-31 17:37:37 +0700

    Submodule libs/poll 679c1ff..231808b:
    > Rename executor to warden

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -1,4 +1,4 @@
-#include <felspar/poll/executor.hpp>
+#include <felspar/poll/warden.poll.hpp>
 #include <pqxx/connection>
 
 #include <iostream>
@@ -6,17 +6,17 @@
 
 namespace {
     felspar::coro::task<pqxx::connection>
-            do_connect(felspar::poll::executor &exec) {
+            do_connect(felspar::poll::warden &ward) {
         std::cout << "Starting connection process..." << std::endl;
         pqxx::connecting cn;
         while (not cn.done()) {
             cn.process();
             if (cn.wait_to_read()) {
                 std::cout << "Waiting for read..." << std::endl;
-                co_await exec.read(cn.sock());
+                co_await ward.read(cn.sock());
             } else if (cn.wait_to_write()) {
                 std::cout << "Waiting for write..." << std::endl;
-                co_await exec.write(cn.sock());
+                co_await ward.write(cn.sock());
             }
         }
         std::cout << "Connected" << std::endl;
@@ -24,16 +24,16 @@ namespace {
     }
 
 
-    felspar::coro::task<void> co_main(felspar::poll::executor &exec) {
-        auto cnx = co_await do_connect(exec);
+    felspar::coro::task<void> co_main(felspar::poll::warden &ward) {
+        auto cnx = co_await do_connect(ward);
     }
 }
 
 
 int main() {
     try {
-        felspar::poll::executor exec;
-        exec.run(co_main);
+        felspar::poll::warden ward;
+        ward.run(co_main);
         return 0;
     } catch (std::exception const &e) {
         std::cerr << "Caught an exception: " << e.what() << std::endl;
